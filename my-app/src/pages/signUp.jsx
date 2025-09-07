3330c3f645b0d4de33552919e24679621ba200de
AhmedElabrass25 <ahmedelabrass2035@gmail.com>
2025-09-07 10:21:36 +0300

    ressponsive(signIn/signUp)

--- my-app/src/pages/signUp.jsx
+++ my-app/src/pages/signUp.jsx
@@ -38,94 +38,101 @@ export default function SignUp() {
   };
 
   return (
-    <div className="mt-5 mb-5 max-w-4xl mx-auto shadow-2xl ring-1 ring-gray-900/10 rounded-xl sm:px-6 lg:px-8 lg:py-8 w-full">
-      <form className=" " onSubmit={handleSignIn}>
-        <div className="flex flex-col @md:flex-row">
-          <div className="">
-            <p className=" mt-3 text-6xl font-bold text-center">Sign Up</p>
-            <div className="gap-y-8 sm:grid-cols-6 flex flex-col justify-center p-8">
-              <div className="sm:col-span-4 ">
-                <label
-                  htmlFor="userName"
-                  className="block text-2xl font-medium text-gray-900"
-                >
-                  User name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="userName"
-                    type="text"
-                    name="userName"
-                    onChange={handleChange}
-                    autoFocus
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
-                      border border-gray-900 placeholder:text-gray-400 
-                      focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none
-                      sm:text-sm leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-4">
-                <label
-                  htmlFor="email"
-                  className="block text-2xl font-medium text-gray-900"
-                >
-                  Email address{" "}
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
-                      border border-gray-900 placeholder:text-gray-400 
-                      focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none
-                      sm:text-sm leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-4">
-                <label
-                  htmlFor="last-name"
-                  className="block text-2xl font-medium text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
-                      border border-gray-900 placeholder:text-gray-400 
-                      focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none
-                      sm:text-sm leading-6"
-                  />
-                </div>
-              </div>
-              <div className="mt-6 flex items-center justify-center">
-                <button
-                  type="submit"
-                  className="w-50% rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-xs 
-            hover:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
-                >
-                  Sign Up
-                </button>
-              </div>
-              <p className=" text-center">
-                Already have account?{" "}
-                <Link className="text-green-600 font-bold" to={"/signin"}>
-                  Sign In
-                </Link>
+    <section className="w-full">
+      <div className="container mx-auto w-full flex items-center justify-center">
+        <form
+          className="mt-5 mb-5 w-full md:w-[600px] shadow-2xl ring-1 ring-gray-900/10 rounded-xl sm:px-6 lg:px-8 lg:py-8"
+          onSubmit={handleSignIn}
+        >
+          <div className="flex flex-col @md:flex-row">
+            <div className="">
+              <p className=" mt-3 text-2xl md:text-6xl font-bold text-center">
+                Sign Up
               </p>
+              <div className="gap-y-8 sm:grid-cols-6 flex flex-col justify-center px-4 py-6">
+                <div className="sm:col-span-4 ">
+                  <label
+                    htmlFor="userName"
+                    className="block text-sm md:text-2xl font-medium text-gray-900"
+                  >
+                    User name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="userName"
+                      type="text"
+                      name="userName"
+                      onChange={handleChange}
+                      autoFocus
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
+                      border border-gray-900 placeholder:text-gray-400 
+                      focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none
+                      sm:text-sm leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm md:text-2xl font-medium text-gray-900"
+                  >
+                    Email address{" "}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
+                      border border-gray-900 placeholder:text-gray-400 
+                      focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none
+                      sm:text-sm leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm md:text-2xl font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 
+                      border border-gray-900 placeholder:text-gray-400 
+                      focus:border-primary focus:ring-2 focus:ring-primary focus:outline-none
+                      sm:text-sm leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="mt-6 flex items-center justify-center">
+                  <button
+                    type="submit"
+                    className="w-50% rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-xs 
+            hover:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+                <p className=" text-center">
+                  Already have account?{" "}
+                  <Link className="text-green-600 font-bold" to={"/signin"}>
+                    Sign In
+                  </Link>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </section>
   );
 }
